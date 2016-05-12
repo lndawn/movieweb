4be48ed68ac74a03d404d487060dc51e4cbee64b
lndawn <lndawn@163.com>
2016-05-12 16:00:15 +0800

    add cross domain by jsonp,request the movie information from douban

--- public/js/admin.js
+++ public/js/admin.js
@@ -15,4 +15,25 @@ $(function(){
 			}
 		})
 	})
+	//jsonp同步豆瓣数据
+	$('#douban').blur(function(){
+		var douban=$(this)
+		var id=douban.val()
+		$.ajax({
+			url:'https://api.douban.com/v2/movie/subject/'+ id,
+			cache:true,
+			type:'get',
+			dataType:'jsonp',
+			crossDomain:true,
+			jsonp:'callback',
+			success:function(data){
+				$('#inputTitle').val(data.title)
+				$('#inputDoctor').val(data.directors[0].name)
+				$('#inputCountry').val(data.countries[0])
+				$('#inputPoster').val(data.images.large)
+				$('#inputYear').val(data.year)
+				$('#inputSummary').val(data.summary)
+			}
+		})
+	})
 })
