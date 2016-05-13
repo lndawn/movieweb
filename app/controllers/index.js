e9271553121b350a88ea8184c27d63837f3a05c8
lndawn <lndawn@163.com>
2016-05-13 14:45:59 +0800

    add list pagination

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,3 +16,32 @@ exports.index=function(req,res){ //request,response
       })
     })
 }
+exports.search=function(req,res){ //request,response
+  var catId=req.query.cat
+  var page=parseInt(req.query.p)
+  var count=2
+  var index=page*count
+  Category
+    .find({_id:catId})
+    .populate({
+      path:'movies',
+      select:'title poster',
+     // options:{limit:2,skip:index} mongoose不支持skip参数查找
+    })
+    .exec(function(err,categories){
+      if(err){
+      console.log(err);
+      }
+      var category=categories[0] || {}
+      var movies=category.movies || []
+      var results=movies.slice(index,index+count)
+      res.render('results',{
+        title:'imooc 结果列表页面',
+        keyword:category.name,
+        currentPage:(page+1),
+        query:catId,
+        totalPage:Math.ceil(movies.length/count),
+        movies:results
+      })
+    })
+}
--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -77,18 +77,31 @@ exports.save=function(req,res){
 	}else{
 		_movie=new Movie(movieObj)
 		var categoryId=_movie.category
-		console.log(movieObj)
+		var categoryName=movieObj.categoryName
 		_movie.save(function(err,movie){
 			if(err){
 				console.log(err)
 			}
-			Category.findById(categoryId,function(err,category){
-				console.log(_movie)
-				category.movies.push(movie._id)
-				category.save(function(err,category){
-					res.redirect('/movie/'+movie._id)
+			if(categoryId){
+				Category.findById(categoryId,function(err,category){
+					category.movies.push(movie._id)
+					category.save(function(err,category){
+						res.redirect('/movie/'+movie._id)
+					})
 				})
-			})
+			}
+			else if(categoryName){
+				var category=new Category({
+					name:categoryName,
+					movies:[movie._id]
+				})
+				category.save(function(err,category){
+					movie.category=category._id
+					movie.save(function(err,movie){
+						res.redirect('/movie/'+movie._id)
+					})
+				})
+			}
 		})
 	}
 }
